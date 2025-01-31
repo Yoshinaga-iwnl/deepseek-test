d2862ba18adb5ef81251e1377d5853e1105ab36d
Yoshinaga-iwnl <yoshinaga.git@gmail.com>
2025-01-31 21:06:31 +0900

    ollamaに書き換えてみる

--- src/deepseek_py/__init__.py
+++ src/deepseek_py/__init__.py
@@ -1,15 +1,11 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer, TextStreamer
+from ollama import chat
+from ollama import ChatResponse
 
-model = AutoModelForCausalLM.from_pretrained("cyberagent/DeepSeek-R1-Distill-Qwen-32B-Japanese", device_map="auto", torch_dtype="auto")
-tokenizer = AutoTokenizer.from_pretrained("cyberagent/DeepSeek-R1-Distill-Qwen-32B-Japanese")
-streamer = TextStreamer(tokenizer, skip_prompt=True, skip_special_tokens=True)
+response: ChatResponse = chat(model='hf.co/bluepen5805/DeepSeek-R1-Distill-Qwen-32B-Japanese-gguf:Q4_K_M', messages=[
+  {
+    'role': 'user',
+    'content': '空が青いのは何故ですか？',
+  },
+])
 
-messages = [
-  {"role": "user", "content": "AIによって私たちの暮らしはどのように変わりますか？"}
-]
-
-input_ids = tokenizer.apply_chat_template(messages, add_generation_prompt=True, return_tensors="pt").to(model.device)
-output_ids = model.generate(input_ids,
-                            max_new_tokens=4096,
-                            temperature=0.7,
-                            streamer=streamer)
+print(response['message']['content'])
